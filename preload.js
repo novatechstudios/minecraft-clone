465ca211c77141477430e1af10ae5c0d2b76d47e
novatechstudios <assassadd6@gmail.com>
2026-07-29 21:30:55 -0400

    Delete preload.js

--- preload.js
+++
@@ -1,5 +0,0 @@
-const { contextBridge, ipcRenderer } = require("electron");
-
-contextBridge.exposeInMainWorld("electronAPI", {
-    toggleFullscreen: () => ipcRenderer.send("toggle-fullscreen")
-});
